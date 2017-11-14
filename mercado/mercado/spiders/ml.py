5a1e562d3b8821df391deec0114fa9febbc29d9b
Adewale Andrade <adewale@oxenti.com.br>
2017-11-14 20:06:39 -0300

    .

--- mercado/mercado/spiders/ml.py
+++ mercado/mercado/spiders/ml.py
@@ -8,12 +8,23 @@ class MlSpider(scrapy.Spider):
     start_urls = ['http://mercadolivre.com.br/']
 
     def start_requests(self):
-    	for url in start_urls:
-    		yeild scrapy.Request(url=url, callback=self.parse)
+    	for url in self.start_urls:
+    		yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        items = response.css("li.results-item")
+        for item in items:
+            item = item.css("a.item__info-link").extract_first()
+            salesField = item.css(".item__status div.item__condition::text").extract_first()
+            if (!salesField) :
+                continue
+            listing = {
+                'url': item.css('a.item_info-link::attr(href)').extract_first(),
+                'title': item.css(".item__title span.main-title::text").extract_first(),
+                'price': item.css(".item__price span.price-fraction::text").extract_first(),
+                'reviews': item.css(".item__reviews div.item__reviews-total::text").extract_first(),
+                'sales': [int(s) for s in salesField.split() if s.isdigit()][0]
+            }
+            print listing
+            exit
+
