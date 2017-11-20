973ccdb2404cd33dcd31de352f8cf9223092e5ed
Adewale Andrade <adewale@oxenti.com.br>
2017-11-20 10:03:38 -0300

    update'

--- mercado/mercado/spiders/ml.py
+++ mercado/mercado/spiders/ml.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import base64
 
 class MlSpider(scrapy.Spider):
     name = 'ml'
     allowed_domains = ['mercadolivre.com.br']
-    start_urls = [
-    	'https://tenis.mercadolivre.com.br/masculino/tenis-masculino',
-    	'https://roupas.mercadolivre.com.br/vestidos/feminino/',
-    	'https://tenis.mercadolivre.com.br/feminino/tenis-feminino',
-    	'https://roupas.mercadolivre.com.br/calcados-roupas/roupas-femininas',
-    	'https://roupas.mercadolivre.com.br/camisetas-blusas/camisetas-blusas_9993122-AMLB*3122*2_9993122-AMLB*3122*2-MMLB33286',
-    	'https://celulares.mercadolivre.com.br/',
-    	'https://roupas.mercadolivre.com.br/calcados-roupas/sandalias-femininas',
-    	'https://relogios.mercadolivre.com.br/pulso/',
-    	'https://lista.mercadolivre.com.br/esportes-aventura-e-acao-armas-propulsao/airsoft',
-    	'https://lista.mercadolivre.com.br/pisos-paredes-e-esquadrias/revestimento-parede/papel-parede',
-    ]
+
+    url_categories = {
+    	'https://tenis.mercadolivre.com.br/masculino/tenis-masculino': 'tenis',
+    	'https://roupas.mercadolivre.com.br/vestidos/feminino/': 'roupas',
+    	'https://tenis.mercadolivre.com.br/feminino/tenis-feminino': 'tenis',
+    	'https://roupas.mercadolivre.com.br/calcados-roupas/roupas-femininas': 'roupas',
+    	'https://roupas.mercadolivre.com.br/camisetas-blusas/camisetas-blusas_9993122-AMLB*3122*2_9993122-AMLB*3122*2-MMLB33286': 'roupas',
+    	'https://celulares.mercadolivre.com.br/': 'celular' ,
+    	'https://roupas.mercadolivre.com.br/calcados-roupas/sandalias-femininas': 'roupas',
+    	'https://relogios.mercadolivre.com.br/pulso/': 'relogios',
+    	'https://lista.mercadolivre.com.br/esportes-aventura-e-acao-armas-propulsao/airsoft': 'airsoft',
+    	'https://lista.mercadolivre.com.br/pisos-paredes-e-esquadrias/revestimento-parede/papel-parede': 'decoracao'
+    }
+
+    start_urls = list(url_categories.keys())
+    currentCategory = url_categories[list(url_categories.keys())[0]]
+
 
     def start_requests(self):
     	for url in self.start_urls:
@@ -34,13 +40,20 @@ class MlSpider(scrapy.Spider):
             	continue
 
             sales = sales[0]
+            url =  item.css('::attr(href)').extract_first()
+            category = self.currentCategory
+            if response.url in self.start_urls:
+            	category = self.url_categories[response.url]
 
+            base64Id = base64.b64encode(bytes(url, 'utf-8'))
             listing = {
-                'url': item.css('::attr(href)').extract_first(),
+            	'id': base64Id,
                 'title': item.css(".item__title span.main-title::text").extract_first(),
+                'url': url,
                 'price': item.css(".item__price span.price-fraction::text").extract_first(),
                 'reviews': item.css(".item__reviews div.item__reviews-total::text").extract_first(),
-                'sales': sales
+                'sales': sales,
+                'category': category 
             }
             yield listing
 
